65f07719d5f74c554b3d5636eae3279e567d2ea5
MichaelK <michaelk05@gmx.de>
2026-04-26 14:48:12 +0200

    Fix creation of Nodes without parent networks

--- src/CreateCommand.cpp
+++ src/CreateCommand.cpp
@@ -12,10 +12,14 @@ void CreateCommand::execute(const std::vector<std::string>& args, NetworkManager
         NetManager.addLAN(std::make_unique<LAN>(args[3]));
     }
     if (args[1] == "PC") {
+        if (args.size() >= 5) {
         for (const auto& lan : NetManager.getLANs()) {
             if (lan->getName() == args[3]) {
                 lan->addNode(std::make_unique<PC>());
             }
         }
+    } else {
+        std::cout << "Failure: You need to specify a parent LAN to create Node." << std::endl;
+    }
     }
 }
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -6,7 +6,9 @@ std::vector<std::string> Parser::tokenize(const std::string& input) {
     std::string current_parameter;
     for(int i = 0; i < input.size(); i++ ) {
         if (input[i] == ' ' || input[i] == '(' || input[i] == ')' || input[i] == ',') {
-            parameters.push_back(current_parameter);
+            if (!current_parameter.empty()) {
+                parameters.push_back(current_parameter);
+            }
             if (input[i] != ' ') { parameters.push_back(std::string(1,input[i])); } // Store ,() but not spaces
 
             current_parameter = "";
