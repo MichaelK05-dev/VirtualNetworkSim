2b035d36406f1a29881012a181b3985e4ba4a608
MichaelK <michaelk05@gmx.de>
2026-05-20 23:46:02 +0200

    Add getters & payload

--- include/EthernetFrame.h
+++ include/EthernetFrame.h
@@ -3,8 +3,14 @@
 
 class EthernetFrame {
     public:
-    EthernetFrame(std::string srcMac, std::string dstMac);
+    EthernetFrame(std::string srcMac, std::string dstMac, std::string payload);
+    std::string getsrcMac();
+    std::string getdstMac();
+    std::string getPayload();
+
+    private:
     std::string srcMac;
     std::string dstMac;
     std::string payload;
+
 };
--- src/EthernetFrame.cpp
+++ src/EthernetFrame.cpp
@@ -1,3 +1,15 @@
 #include "EthernetFrame.h"
 
-EthernetFrame::EthernetFrame(std::string srcMac, std::string dstMac) : srcMac(srcMac), dstMac(dstMac) {}
+EthernetFrame::EthernetFrame(std::string srcMac, std::string dstMac, std::string payload) : srcMac{srcMac}, dstMac{dstMac}, payload{payload} {}
+
+std::string EthernetFrame::getsrcMac() {
+    return srcMac;
+}
+
+std::string EthernetFrame::getdstMac() {
+    return dstMac;
+}
+
+std::string EthernetFrame::getPayload() {
+    return payload;
+}
