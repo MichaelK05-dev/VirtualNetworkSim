6f433fae545fe4cda60deea3bb9d2f29566487e7
MichaelK <michaelk05@gmx.de>
2026-05-26 00:06:15 +0200

    Enhance error handling in StepCommand

--- src/commands/StepCommand.cpp
+++ src/commands/StepCommand.cpp
@@ -1,8 +1,20 @@
 #include "commands/StepCommand.h"
 #include "NetworkManager.h"
 #include <string>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
 
 void StepCommand::execute(const std::vector<std::string>& args, NetworkManager& NetManager) {
-    NetManager.simulate(std::stoi(args[1]));
+    try {
+        size_t processed_chars = 0;
+    NetManager.simulate(std::stoi(args[1], &processed_chars));
+
+    if (processed_chars != args[1].size()) {
+        throw std::invalid_argument("Please provide an integer value.");
+    }
+    } catch (const std::exception& e) {
+        std::cout << "Invalid arguments for STEP. Please provide an integer value." << std::endl;
+    }
 
 }
